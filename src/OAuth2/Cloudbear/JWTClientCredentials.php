ed9991a615c6fcba6e1a1911a09928951c080949
Marlin Cremers <mcremers@cloudbear.nl>
2020-10-06 22:56:46 +0200

    Move random_str method to own file

--- src/OAuth2/Cloudbear/JWTClientCredentials.php
+++ src/OAuth2/Cloudbear/JWTClientCredentials.php
@@ -31,18 +31,3 @@ class JWTClientCredentials extends ClientCredentials
         return (string)$token;
     }
 }
-
-function random_str(
-    int $length = 64,
-    string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-): string {
-    if ($length < 1) {
-        throw new \RangeException("Length must be a positive integer");
-    }
-    $pieces = [];
-    $max = mb_strlen($keyspace, '8bit') - 1;
-    for ($i = 0; $i < $length; ++$i) {
-        $pieces []= $keyspace[random_int(0, $max)];
-    }
-    return implode('', $pieces);
-}
