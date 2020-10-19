06ea374c51c0e0e7e309551a35ca6c117fd7f035
Marlin Cremers <mcremers@cloudbear.nl>
2020-10-19 18:05:52 +0200

    Add Cloud-Init and option to request IPv4 address

--- src/VirtualMachines/V1alpha1/Instance.php
+++ src/VirtualMachines/V1alpha1/Instance.php
@@ -10,8 +10,14 @@ class Instance
     const FIELD_ROOT_DISK = 'root_disk';
     const FIELD_ADDRESS = 'address';
     const FIELD_SSH_KEYS = 'ssh_keys';
+    const FIELD_CI_USERNAME = 'ci_username';
+    const FIELD_CI_PASSWORD = 'ci_password';
+    const FIELD_CI_USERDATA = 'ci_userdata';
     const FIELD_STATE = 'state';
     const FIELD_LABELS = 'labels';
+    const FIELD_OPTIONS = 'options';
+
+    const OPTION_REQUEST_IPV4 = 'request_ipv4';
 
     /**
      * @var \Cbws\VirtualMachines\Grpc\V1alpha1\Instance
@@ -160,6 +166,11 @@ class Instance
         return $this;
     }
 
+    public function getAdditionalAddresses(): array
+    {
+        return iterator_to_array($this->object->getAdditionalAddress());
+    }
+
     public function getSSHKeys(): array
     {
         return iterator_to_array($this->object->getSshKeys());
@@ -177,6 +188,69 @@ class Instance
 
         $this->object->setSshKeys($sshKeys);
         $this->updatedFields[] = self::FIELD_SSH_KEYS;
+
+        return $this;
+    }
+
+    public function getCIUsername(): ?string
+    {
+        return ($this->object->getCiUsername() != "") ? $this->object->getCiUsername() : null;
+    }
+
+    public function withCIUsername(string $username = null): self
+    {
+        if ($username === $this->getCIUsername()) {
+            return $this;
+        }
+
+        if (is_null($username)) {
+            $username = '';
+        }
+
+        $this->object->setCiUsername($username);
+        $this->updatedFields[] = self::FIELD_CI_USERNAME;
+
+        return $this;
+    }
+
+    public function getCIPassword(): ?string
+    {
+        return ($this->object->getCiPassword() != "") ? $this->object->getCiPassword() : null;
+    }
+
+    public function withCIPassword(string $password = null): self
+    {
+        if ($password === $this->getCIPassword()) {
+            return $this;
+        }
+
+        if (is_null($password)) {
+            $password = '';
+        }
+
+        $this->object->setCiPassword($password);
+        $this->updatedFields[] = self::FIELD_CI_PASSWORD;
+
+        return $this;
+    }
+
+    public function getCIUserdata(): ?string
+    {
+        return ($this->object->getCiUserdata() != "") ? $this->object->getCiUserdata() : null;
+    }
+
+    public function withCIUserdata(string $userdata = null): self
+    {
+        if ($userdata === $this->getCIUserdata()) {
+            return $this;
+        }
+
+        if (is_null($userdata)) {
+            $userdata = '';
+        }
+
+        $this->object->setCiUserdata($userdata);
+        $this->updatedFields[] = self::FIELD_CI_USERDATA;
 
         return $this;
     }
@@ -212,6 +286,50 @@ class Instance
         return $this;
     }
 
+    public function getOptions(): array
+    {
+        $options = [];
+        foreach ($this->object->getOptions() as $key => $value) {
+            $options[$key] = $value;
+        }
+
+        return $options;
+    }
+
+    public function withOptions(array $options = null): self
+    {
+        if ($options === $this->getOptions()) {
+            return $this;
+        }
+
+        if (is_null($options)) {
+            $options = [];
+        }
+
+        $this->object->setOptions($options);
+        $this->updatedFields[] = self::FIELD_OPTIONS;
+
+        return $this;
+    }
+
+    public function getRequestIPv4(): ?bool
+    {
+        $options = $this->getOptions();
+        if (!isset($options[self::OPTION_REQUEST_IPV4])) {
+            return null;
+        }
+
+        return $options[self::OPTION_REQUEST_IPV4];
+    }
+
+    public function withRequestIPv4(bool $request = true): self
+    {
+        $options = $this->getOptions();
+        $options[self::OPTION_REQUEST_IPV4] = ($request) ? 'true' : 'false';
+
+        return $this->withOptions($options);
+    }
+
     public function toGrpc(): \Cbws\VirtualMachines\Grpc\V1alpha1\Instance
     {
         return $this->object;
@@ -232,9 +350,14 @@ class Instance
             'memory' => $this->getMemory() . $this->isUpdated('memory'),
             'rootDisk' => $this->getRootDisk() . $this->isUpdated('root_disk'),
             'address' => $this->getAddress() . $this->isUpdated('address'),
+            'additionalAddresses' => $this->getAdditionalAddresses(),
             'sshKeys' => $this->getSSHKeys(),
+            'ciUsername' => $this->getCIUsername() . $this->isUpdated(self::FIELD_CI_USERNAME),
+            'ciPassword' => $this->getCIPassword(),
+            'ciUserdata' => $this->getCIUserdata() . $this->isUpdated(self::FIELD_CI_USERDATA),
             'state' => $this->getState(),
             'labels' => $this->getLabels(),
+            'options' => $this->getOptions(),
         ];
 
         if (!empty($this->updatedFields())) {
